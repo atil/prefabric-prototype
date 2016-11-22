297d585a1ce9badead9d76344a7faaa47711c611
atil <hyperion91@gmail.com>
2016-11-23 01:10:25 +0300

    Basic level loading;

--- Assets/Scripts/PfResources.cs
+++ Assets/Scripts/PfResources.cs
@@ -1,10 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
+using SimpleJSON;
+using System.Collections.Generic;
 
-namespace Shooter
+namespace Prefabric
 {
+    public enum PfResourceType
+    {
+        WhiteTile,
+    }
+
 	public static class PfResources
 	{
-		 
+        private static List<string> _levelPaths = new List<string>();
+        private static Dictionary<PfResourceType, string> _resourcePaths = new Dictionary<PfResourceType, string>();
+
+		static PfResources()
+        {
+            var json = JSON.Parse(Resources.Load<TextAsset>("levelPaths").text);
+            foreach (JSONNode pathString in json["paths"].AsArray)
+            {
+                _levelPaths.Add(pathString.Value);
+            }
+
+            _resourcePaths.Add(PfResourceType.WhiteTile, "Prefabs/Tiles/WhiteTile");
+        }
+
+        public static string LevelStringOf(int index)
+        {
+            var textAsset = Resources.Load<TextAsset>(_levelPaths[index]);
+            if (textAsset == null)
+            {
+                Debug.LogError("No level found for index : " + 0);
+                return string.Empty; 
+            }
+            return textAsset.text;
+        }
+
+        public static T Load<T>(PfResourceType resType) where T : Object
+        {
+            if (!_resourcePaths.ContainsKey(resType))
+            {
+                Debug.LogError("Resource doesn't exist : " + resType);
+                return null;
+            }
+
+            return Resources.Load<T>(_resourcePaths[resType]);
+        }
 	}
 }
--- Assets/Scripts/World/MapManager.cs
+++ Assets/Scripts/World/MapManager.cs
@@ -1,22 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using SimpleJSON;
 
 namespace Prefabric
 {
     public class MapManager
     {
+        private class Tile
+        {
+            private GameObject _go;
+
+            public Tile(GameObject go)
+            {
+                _go = go;
+            }
+        }
+        
         private readonly ControllerBase _controller;
         private readonly List<AgentBase> _agents = new List<AgentBase>();
         private readonly PlayerAgent _player;
+        private List<Tile> _tiles;
 
-        public MapManager(uint lvlNum, ControllerBase controller, List<AgentBase> agents)
+        public MapManager(int lvlNum, ControllerBase controller, List<AgentBase> agents)
         {
             _controller = controller;
             _agents = agents;
             _player = _agents.Find(x => x is PlayerAgent) as PlayerAgent;
+
+            _tiles = LoadLevel(lvlNum);
         }
 
+        private List<Tile> LoadLevel(int lvlNum)
+        {
+            var tiles = new List<Tile>();
+            var whiteTilePrefab = PfResources.Load<GameObject>(PfResourceType.WhiteTile);
 
+            var lvlJson = JSON.Parse(PfResources.LevelStringOf(lvlNum));
+            foreach (JSONClass tileEntry in lvlJson["tiles"].AsArray)
+            {
+                var tilePos = new Vector3(tileEntry["x"].AsInt, tileEntry["y"].AsInt, tileEntry["z"].AsInt);
+                var tileGo = Object.Instantiate(whiteTilePrefab, tilePos, Quaternion.identity) as GameObject;
+
+                tiles.Add(new Tile(tileGo));
+            }
+
+            return tiles;
+        }
     }
 }
