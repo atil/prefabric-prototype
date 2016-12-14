c278ec47f03d72929384cf0235345e0c9565670f
atil <hyperion91@gmail.com>
2016-12-14 23:22:42 +0300

    Some documentation;

--- Assets/Scripts/World/GameSceneArgs.cs
+++ Assets/Scripts/World/GameSceneArgs.cs
@@ -9,11 +9,29 @@ namespace Prefabric
 {
     // A PlayerPrefs-like usage
     // Better to have it on file
+
+    /// <summary>
+    /// This data can be though of parameters to "GameScene's constructor"
+    /// I try to make GameScene as seperate from the rest of the game as possible
+    /// GameScene is init'ed purely based on GameSceneArgs
+    /// </summary>
     public class GameSceneArgs
     {
+        /// <summary>
+        /// GameScene loads this level, no matter the context
+        /// New game, load game, edit mode etc..
+        /// </summary>
         public string LevelName { get; private set; }
+
+        /// <summary>
+        /// Being in edit mode requires some extra logic in GameScene
+        /// </summary>
         public bool IsEditMode { get; private set; }
 
+        /// <summary>
+        /// Load data from file and parse it
+        /// </summary>
+        /// <returns></returns>
         public static GameSceneArgs Load()
         {
             var gameSceneArgsJson = JSON.Parse(PfResources.LoadStringAt("gameSceneArgs.json"));
@@ -24,6 +42,11 @@ namespace Prefabric
             };
         }
 
+        /// <summary>
+        /// Save the data to be used init'ing GameScene
+        /// </summary>
+        /// <param name="lvlName"></param>
+        /// <param name="isEditMode"></param>
         public static void Write(string lvlName, bool isEditMode)
         {
             var gameSceneArgsJson = JSON.Parse(PfResources.LoadStringAt("gameSceneArgs.json"));
